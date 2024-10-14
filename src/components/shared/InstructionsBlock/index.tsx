8e78fbd9173a1ab2461d2f17e395c95e373626e1
wizzzx <alexkryukovzzx@gmail.com>
2024-10-14 18:08:53 +0300

    second section almost done

--- src/components/shared/InstructionsBlock/index.tsx
+++ src/components/shared/InstructionsBlock/index.tsx
@@ -1,20 +1,29 @@
-'use client'
+"use client";
 
 import React from "react";
-import styles from './index.module.scss'
-import {BlockTitle} from "@/components/ui/BlockTitle";
-import {Container} from "@/components/shared/Container";
+import styles from "./index.module.scss";
+import { BlockTitle } from "@/components/ui/BlockTitle";
+import { Container } from "@/components/shared/Container";
 import Image from "next/image";
+import {StepList} from "@/components/ui/StepList";
 
 export const InstructionsBlock: React.FC = () => {
-    return (
-        <Container className={styles.container}>
-            <BlockTitle className={styles.title_text} title={"Как получить займ онлайн на карту?"} />
-            <Container>
-                <Image src='/assets/images/3035.svg' alt='loan calculator on mobile screen' width={337.98} height={685.91} />
-
-            </Container>
+  return (
+    <Container className={styles.container}>
+      <BlockTitle
+        className={styles.title_text}
+        title={"Как получить займ онлайн на карту?"}
+      />
+        <Container className={styles.sub_container}>
+            <Image
+                src="/assets/images/3034.svg"
+                alt="loan calculator on mobile screen"
+                width={337.98}
+                height={685.91}
+                className={styles.picture}
+            />
+            <StepList texts={['Заполните короткую анкету', 'Примите предложение кредитора', 'Получите деньги']}/>
         </Container>
-    )
-}
-
+    </Container>
+  );
+};
--- src/components/ui/StepList/index.tsx
+++ src/components/ui/StepList/index.tsx
@@ -9,19 +9,39 @@ import {Container} from "@/components/shared/Container";
 
 interface Props {
     className?: string,
-
+    texts?: string[]
 }
 
 const StepNumbers = [
     {
-
+        src: '/assets/icons/ui/1st_step.svg',
+        alt: '1st step icon',
+        width: 66.29,
+        height: 66.29
+    },
+    {
+        src: '/assets/icons/ui/2nd_step.svg',
+        alt: '2nd step icon',
+        width: 66.29,
+        height: 66.29
+    },
+    {
+       src: '/assets/icons/ui/3rd_step.svg',
+        alt: '3rd step icon',
+        width: 66.29,
+        height: 66.29
     }
 ]
 
-export const StepList: React.FC<Props> = ({className, }) => {
+export const StepList: React.FC<Props> = ({className, texts = [] }) => {
     return (
-        <Container className={styles.container}>
-
+        <Container className={cn(styles.container, className)}>
+            {StepNumbers.map((item, index) => (
+                <div key={index} className={styles.step_container}>
+                    <Image src={item.src} alt={item.alt} width={item.width} height={item.height} />
+                    {texts[index] && <p className={styles.step_text}>{texts[index]}</p>}
+                </div>
+            ))}
         </Container>
     )
 }
