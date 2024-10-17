99385318d6d8c10341ad3d58a378a9314b72940e
wizzzx <alexkryukovzzx@gmail.com>
2024-10-17 14:20:38 +0300

    range slider struggle

---
+++ src/components/shared/GuaranteeBlock/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React from "react";
+import styles from './index.module.scss'
+import Image from "next/image";
+import {Container} from "@/components/shared/Container";
+
+
+export const GuaranteeBlock: React.FC = () => {
+    return (
+        <Container className={styles.container}>
+            <h2 className={styles.section_header}>Займ на карту онлайн за 5 минут</h2>
+            <div className={styles.section_container}>
+                <Image src={'/assets/images/hands.svg'} alt={'Hands holding a phone picture'} width={864} height={911}/>
+            {/*    drop down component*/}
+            </div>
+
+        </Container>
+
+
+    )
+}
+
+
