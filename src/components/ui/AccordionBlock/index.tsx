752444eec6740967ebfc0d0e31641d7c67a4c342
wizzzx <alexkryukovzzx@gmail.com>
2024-10-22 11:58:27 +0300

    accordion done

--- src/components/ui/AccordionBlock/index.tsx
+++ src/components/ui/AccordionBlock/index.tsx
@@ -5,7 +5,6 @@ import styles from "./index.module.scss";
 import cn from "classnames";
 import Image from "next/image";
 
-
 interface AccordionBlockProps {
   className?: string;
   id?: string;
@@ -16,53 +15,56 @@ interface AccordionBlockProps {
 }
 
 interface Props {
-  className?: string
+  className?: string;
+  data: AccordionBlockProps[];
 }
 
-const GuaranteeData: AccordionBlockProps[] = [
-  {
-    id: '1',
-    title: "Комфорт",
-    content: "МигКредит - один из лидеров среди крупнейших микрофинансовых организаций в России и ежедневно помогает миллионам заемщиков по всей стране. Вы можете моментально оформить срочную заявку на займ онлайн на карту или наличными - за 5 минут. Подать заявку можно через мобильное приложение или официальный сайт компании.",
-  },
-  {
-    id: '2',
-    title: "Гарантия",
-    content: "МигКредит - один из лидеров среди крупнейших микрофинансовых организаций в России и ежедневно помогает миллионам заемщиков по всей стране. Вы можете моментально оформить срочную заявку на займ онлайн на карту или наличными - за 5 минут. Подать заявку можно через мобильное приложение или официальный сайт компании.",
-  },
-  {
-    id: '3',
-    title: "Безопасность",
-    content: "МигКредит - один из лидеров среди крупнейших микрофинансовых организаций в России и ежедневно помогает миллионам заемщиков по всей стране. Вы можете моментально оформить срочную заявку на займ онлайн на карту или наличными - за 5 минут. Подать заявку можно через мобильное приложение или официальный сайт компании.",
-  },
-  {
-    id: '4',
-    title: "Первый займ без процентов",
-    content: "МигКредит - один из лидеров среди крупнейших микрофинансовых организаций в России и ежедневно помогает миллионам заемщиков по всей стране. Вы можете моментально оформить срочную заявку на займ онлайн на карту или наличными - за 5 минут. Подать заявку можно через мобильное приложение или официальный сайт компании.",
-  },
-  {
-    id: '5',
-    title: "Ремонт будок прицепов",
-    content: "МигКредит - один из лидеров среди крупнейших микрофинансовых организаций в России и ежедневно помогает миллионам заемщиков по всей стране. Вы можете моментально оформить срочную заявку на займ онлайн на карту или наличными - за 5 минут. Подать заявку можно через мобильное приложение или официальный сайт компании.",
-  },
-];
-
-const AccordionItem: React.FC<AccordionBlockProps & { isOpen: boolean, toggleAccordion: () => void }> = ({ className, title, content, isOpen, toggleAccordion }) => {
-
+const AccordionItem: React.FC<AccordionBlockProps> = ({
+  className,
+  title,
+  content,
+  isOpen,
+  toggleAccordion,
+}) => {
   return (
-    <div className={cn(styles.accordion_item, className)}>
-      <div className={cn(styles.accordion_item_title, className, {[styles.accordion_item_title_open] : isOpen})} onClick={toggleAccordion}>
-        <h3>{title}</h3>
-        {/* <span>{isOpen ? '-' : '+'}</span> */}
-        
-      </div>
-      {isOpen && <div className={cn(styles.accordion_item_content, className, {[styles.accordion_item_content_open] : isOpen})}>{content}</div>}
+    <div
+      className={cn(
+        className,
+        {
+          [styles.accordion_item_open]: isOpen,
+        },
+        {
+          [styles.accordion_item]: !isOpen,
+        },
+      )}
+      onClick={toggleAccordion}
+    >
+      <h3>{title}</h3>
+      {!isOpen && (
+        <Image
+          src={"/assets/icons/ui/accordion_arrow.svg"}
+          alt={"accordion arrow"}
+          className={styles.arrow_icon}
+          width={40}
+          height={40}
+        />
+      )}
+      {isOpen && (
+        <div
+          className={cn(className, {
+            [styles.accordion_item_content_open]: isOpen,
+          })}
+        >
+          <div>
+            <p className={styles.content}>{content}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-
-export const AccordionBlock: React.FC<Props> = ({ className }) => {
+export const AccordionBlock: React.FC<Props> = ({ className, data }) => {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   const toggleAccordion = (index: number) => {
@@ -70,8 +72,8 @@ export const AccordionBlock: React.FC<Props> = ({ className }) => {
   };
 
   return (
-    <div className={cn(styles.accordion, className)}>
-      {GuaranteeData.map((item, index) => (
+    <div className={cn(styles.accordion_container, className)}>
+      {data.map((item, index) => (
         <AccordionItem
           key={item.id}
           title={item.title}
