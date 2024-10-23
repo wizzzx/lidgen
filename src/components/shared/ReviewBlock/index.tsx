b3277743f85c727957767c1494daadfcc496e435
wizzzx <alexkryukovzzx@gmail.com>
2024-10-23 12:59:26 +0300

    done review block, starting customer rating form block

--- src/components/shared/ReviewBlock/index.tsx
+++ src/components/shared/ReviewBlock/index.tsx
@@ -4,12 +4,13 @@ import React from "react";
 import styles from "./index.module.scss";
 import { Container } from "@/components/shared/Container";
 import Image from "next/image";
+import Link from "next/link";
 
 const ReviewData = [
   {
     id: 1,
     title: "Константин Душный",
-    text: "Очень довольна условиями предоставления займов и обслуживанием. Сотрудники вежливы и внимательны. Всегда есть различные бонусы и акции. Есть возможность совершать все операции через мобильное приложение...",
+    text: "Очень довольна условиями предоставления займов и обслуживанием. Сотрудники вежливы и внимательны. Всегда есть различные бонусы и акции. Есть возможность совершать все операции через мобильное приложение...Ещё",
     date: "02.08.2023",
     stars: 4,
   },
@@ -63,21 +64,40 @@ export const ReviewBlock: React.FC = () => {
   return (
     <Container className={styles.container}>
       <div className={styles.review_container}>
-        <h3>Отзывы наших клиентов</h3>
-        <p>Поделитесь своим мнением о сервисе</p>
+        <div className={styles.review_subcontainer}>
+          <h3 className={styles.section_header}>Отзывы наших клиентов</h3>
+          <p className={styles.section_subheader}>
+            Поделитесь своим мнением о сервисе
+          </p>
+        </div>
         <div className={styles.review_cards_container}>
           {ReviewData.map((item) => (
-            <div key={item.id}>
-              <h4 className={styles.name}>{item.title}</h4>
-              <div className={styles.date_stars_container}>
-                <time>{item.date}</time>
-                <StarRating stars={item.stars} />
+            <div key={item.id} className={styles.review_card}>
+              <div className={styles.review_card_container}>
+                <h4 className={styles.name}>{item.title}</h4>
+                <div className={styles.date_stars_container}>
+                  <time>{item.date}</time>
+                  <StarRating stars={item.stars} />
+                </div>
               </div>
+              <p className={styles.review_text}>{item.text}</p>
             </div>
           ))}
+          <div className={styles.review_link_container}>
+            <Image
+              src={"/assets/icons/ui/review_arrow.svg"}
+              alt={"arrow"}
+              width={20}
+              height={10}
+              className={styles.arrow}
+            />
+            <Link href={"/reviews"} className={styles.link}>
+              Читать все отзывы
+            </Link>
+          </div>
         </div>
       </div>
-      <div className={styles.review_form_container}></div>
+      <div className={styles.review_form_container}> wasd</div>
     </Container>
   );
 };
