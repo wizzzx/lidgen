b27ce6e669083260a09146c6203380a792d3231b
wizzzx <alexkryukovzzx@gmail.com>
2024-12-20 16:42:53 +0300

    work in progress

--- src/app/new_land_1/page.tsx
+++ src/app/new_land_1/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import styles from "./page.module.scss";
 import { TitleBlockNewVariant } from "@/components/shared/TitleBlockNewVariant";
 import { LinkSlider } from "@/components/shared/LinkSlider";
+import Image from "next/image";
 
 export default function LandingOne() {
   return (
@@ -14,6 +15,54 @@ export default function LandingOne() {
       </p>
       <TitleBlockNewVariant />
       <LinkSlider />
+      <div className={styles.text_container}>
+        <div className={styles.header_icons_container}>
+          <h2 className={styles.header}>Займ онлайн - ваш осознанный выбор</h2>
+          <div className={styles.icon_container}>
+            <Image
+              src={"/assets/icons/ui/bank_navy.svg"}
+              alt={"bank"}
+              width={38}
+              height={38}
+            />
+            <Image
+              src={"/assets/icons/ui/arrow_navy.svg"}
+              alt={"arrow"}
+              width={14}
+              height={14}
+            />
+            <Image
+              src={"/assets/icons/ui/clock_navy.svg"}
+              alt={"clock"}
+              width={38}
+              height={38}
+            />
+            <Image
+              src={"/assets/icons/ui/arrow_navy.svg"}
+              alt={"arrow"}
+              width={14}
+              height={14}
+            />
+            <Image
+              src={"/assets/icons/ui/thumbsup_navy.svg"}
+              alt={"thumb"}
+              width={38}
+              height={38}
+            />
+          </div>
+        </div>
+        <div className={styles.content_container}>
+          <h3 className={styles.section_subheader}>
+            Получить займ на карту онлайн достаточно легко
+          </h3>
+          <p className={styles.section_paragraph}>
+            Важен выбор финансового партнёра, который поможет получить деньги
+            быстро, бережно обращается с вашими персональными данными, даёт вам
+            только проверенный порядочные микрофинансовые компании, имеющие
+            стандарт качества и хорошую репутацию.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
