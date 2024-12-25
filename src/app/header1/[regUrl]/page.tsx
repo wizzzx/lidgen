cb4cc1d4bf3e8194a6d05d6bc02c485b898398ce
wizzzx <alexkryukovzzx@gmail.com>
2024-12-25 17:26:09 +0300

    work in progress

--- src/app/header1/[regUrl]/page.tsx
+++ src/app/header1/[regUrl]/page.tsx
@@ -1,40 +1,40 @@
-"use client";
-
-import { SelectedMfoInfo } from "@/components/shared/SelectedMfoInfo";
-import { TitleBlock } from "@/components/shared/TitleBlock";
-import { MapBlock } from "@/components/shared/MapBlock";
-import { InstructionsBlock } from "@/components/shared/InstructionsBlock";
-import { TermsBlock } from "@/components/shared/TermsBlock";
-import { ReviewBlock } from "@/components/shared/ReviewBlock";
-import { GuaranteeBlock } from "@/components/shared/GuaranteeBlock";
-import { OtherOffersBlock } from "@/components/shared/OtherOffersBlock";
-import * as React from "react";
-import data from "../../../data/mfo_data.json";
-import { notFound } from "next/navigation";
-
-interface Props {
-  params: {
-    regUrl: string;
-  };
-}
-
-export default function MfoPage({ params }: Props) {
-  const selectedMfo = data.find((mfo) => mfo.regUrl === params.regUrl);
-
-  if (!selectedMfo) {
-    notFound();
-  }
-
-  return (
-    <div>
-      <TitleBlock />
-      <SelectedMfoInfo data={selectedMfo} />
-      <MapBlock />
-      <InstructionsBlock />
-      <TermsBlock />
-      <ReviewBlock />
-      <GuaranteeBlock />
-      <OtherOffersBlock />
-    </div>
-  );
-}
+// "use client";
+//
+// import { SelectedMfoInfo } from "@/components/shared/SelectedMfoInfo";
+// import { TitleBlock } from "@/components/shared/TitleBlock";
+// import { MapBlock } from "@/components/shared/MapBlock";
+// import { InstructionsBlock } from "@/components/shared/InstructionsBlock";
+// import { TermsBlock } from "@/components/shared/TermsBlock";
+// import { ReviewBlock } from "@/components/shared/ReviewBlock";
+// import { GuaranteeBlock } from "@/components/shared/GuaranteeBlock";
+// import { OtherOffersBlock } from "@/components/shared/OtherOffersBlock";
+// import * as React from "react";
+// import data from "../../../data/mfo_data.json";
+// import { notFound } from "next/navigation";
+//
+// interface Props {
+//   params: {
+//     regUrl: string;
+//   };
+// }
+//
+// export default function MfoPage({ params }: Props) {
+//   const selectedMfo = data.find((mfo) => mfo.regUrl === params.regUrl);
+//
+//   if (!selectedMfo) {
+//     notFound();
+//   }
+//
+//   return (
+//     <div>
+//       <TitleBlock />
+//       <SelectedMfoInfo data={selectedMfo} />
+//       <MapBlock />
+//       <InstructionsBlock />
+//       <TermsBlock />
+//       <ReviewBlock />
+//       <GuaranteeBlock />
+//       <OtherOffersBlock />
+//     </div>
+//   );
+// }
